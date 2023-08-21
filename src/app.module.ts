453cbc6c8e18640d81b4a86d8f173e12a567c648
macbook air <mvoodivood@yandex.ru>
2023-08-21 17:45:06 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+    imports: [UserModule, PrismaModule, AuthModule],
+})
+export class AppModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '@prisma/prisma.service';
+import {genSaltSync, hashSync, } from 'bcrypt'
+
+@Injectable()
+export class UserService {
+    constructor(private prismaService: PrismaService){}
+
+    save(user: Partial<User>){
+        console.log(user)
+        return this.prismaService.user.create({
+            data: {
+                email: user.email,
+                password: this.hashPassword(user.password),
+                roles: ["USER"]
+            }
+        })
+    }
+
+    findOne(idOrEmail: string){
+        return this.prismaService.user.findFirst({where: {
+            OR: [
+                {id: idOrEmail},
+                {email: idOrEmail}
+            ]
+        }})
+    }
+    
+    delite(id: string){
+        return this.prismaService.user.delete({where: {id}})
+    }
+
+    private hashPassword(password: string){
+        return hashSync(password, genSaltSync(10))    
+    }
+}
